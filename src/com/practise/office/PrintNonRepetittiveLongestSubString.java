fd37e45885d60e9de4ea4bb7ca1d1711d3e6c548
rajaguru <macguru@hotmail.com>
2019-08-31 15:10:54 +0530

    Fix in StringRepLongSubString by changing the logics

--- src/com/practise/office/PrintNonRepetittiveLongestSubString.java
+++ src/com/practise/office/PrintNonRepetittiveLongestSubString.java
@@ -1,39 +1,35 @@
 package com.practise.office;
 
 public class PrintNonRepetittiveLongestSubString {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String inputValue = "abbccabcdefghaa";
-		int index = -1;
-		boolean isFirstQueue = true;
-		int l = inputValue.length();
-		for (int i = 0; i < l - 1; i++) {
-			index = -1;
-			for (int j = i + 1; j < l; j++) {
-				if (inputValue.charAt(i) == inputValue.charAt(j)) {
-					index = j;
+		String input = "abcd";
+		//System.out.println(input.substring(1,2));
+		String output = "";
+		for(int i=0 ; i<input.length() ; i++){
+			for(int j=i+1 ; j<=input.length() ; j++){
+				String value = input.substring(i,j);
+				if(value.length() < input.length() && !hasDuplicates(value) && value.length() > output.length()){
+					output = value;
+				}
+			}
+		}
+		System.out.println(output);
+	}
+	
+	
+	public static boolean hasDuplicates(String input){
+		boolean hasDuplicate = false;
+		for(int i=0 ; i<input.length() ; i++){
+			for(int j=i+1 ; j<input.length() ; j++){
+				if(input.charAt(i) == input.charAt(j)){
+					hasDuplicate = true;
 					break;
 				}
 			}
-			if(index == -1 && isFirstQueue) {
-				System.out.println(inputValue.substring(1, l));
-				break;
-			}else if(index == -1){
-				System.out.println(inputValue);
-				break;
-			}
-			if(inputValue.substring(0, index).length() < inputValue.substring(index, l).length()) {
-				inputValue = inputValue.substring(index, l);
-				l = inputValue.length();
-				i = -1;
-				isFirstQueue = false;
-			}else {
-				System.out.println(inputValue.substring(0, index));
+			if(hasDuplicate){
 				break;
 			}
 		}
+		return hasDuplicate;
 	}
-	
-
 }
